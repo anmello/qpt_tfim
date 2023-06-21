c767178d4c2ae9e33ff449585156d29f70cc7328
grossiM <michele.grossi89@gmail.com>
2023-06-21 11:01:32 +0200

    start testing

--- batch_run.sh
+++ batch_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #   f 0 noiseless simulation, 1 noisy simulation, 2 real backend), J (=1 antiferro, =-1 ferro), B (magnetic field) and n (number of qubits in the system)
-J=-1
+J=1
+f=1
+
 for B in -0.2 0 0.2
 do
-	python main.py -f 0 -J $J -B $B -n 5  > output_log/output_${B}_${J}.txt &
+	python main.py -f $f -J $J -B $B -n 5  > output_log/output_${f}_${B}_${J}.txt &
 done
--- main.py
+++ main.py
@@ -160,13 +160,12 @@ elif flag == 1:
         options = Options()
         options.simulator = {
             "noise_model": noise_model,
-            "basis_gates": fake_backend.configuration().basis_gates,
-            "coupling_map": fake_backend.configuration().coupling_map,
+            "coupling_map": fake_backend.coupling_map,
             "seed_simulator": 42
         }
 
         # Set number of shots, optimization_level and resilience_level
-        options.execution.shots = 2000
+        options.execution.shots = 5000
         options.optimization_level = 3
         options.resilience_level = 2
 
