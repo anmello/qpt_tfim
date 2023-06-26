2bdf80c458a5e0150863fbb49c6682797bb6f386
anmello <95907305+anmello@users.noreply.github.com>
2023-06-26 13:52:40 +0200

    magn print fixed

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ if flag == 0:
         tmp_title = str(flag) + "_" + str(J) + "_" + str(h) + "_" + "_" + str(nqubits) + ".txt"
         values = open(tmp_title, "w+")
         print(result.optimal_value, file=values)
-        print(result.aux_operators_evaluated, file=values)
+        print((result.aux_operators_evaluated)[0][0], file=values)
 
         values.close()
 
@@ -160,7 +160,7 @@ elif flag == 1:
         options = Options()
         options.simulator = {
             "noise_model": noise_model,
-            #"coupling_map": fake_backend.coupling_map,
+            "coupling_map": fake_backend.coupling_map,
             "seed_simulator": 42
         }
 
@@ -176,14 +176,14 @@ elif flag == 1:
         H = ham_generator(nqubits, h, J)
         result = vqe.compute_minimum_eigenvalue(H, aux_operators=[M])
 
-        print("Experiment complete.".ljust(30))
-        print(f"GS: {result.optimal_value}")
-        print(f"Magn:{result.aux_operators_evaluated}")
+        # print("Experiment complete.".ljust(30))
+        # print(f"GS: {result.optimal_value}")
+        # print(f"Magn:{result.aux_operators_evaluated}")
 
         tmp_title = str(flag) + "_" + str(J) + "_" + str(h) + "_" + "_" + str(nqubits) + ".txt"
         values = open(tmp_title, "w+")
         print(result.optimal_value, file=values)
-        print(result.aux_operators_evaluated, file=values)
+        print((result.aux_operators_evaluated)[0][0], file=values)
 
         values.close()
 
@@ -218,7 +218,7 @@ elif flag == 2:
         tmp_title = str(flag) + "_" + str(J) + "_" + str(h) + "_" + "_" + str(nqubits) + ".txt"
         values = open(tmp_title, "w+")
         print(result.optimal_value, file=values)
-        print(result.aux_operators_evaluated, file=values)
+        print((result.aux_operators_evaluated)[0][0], file=values)
 
         values.close()
 
