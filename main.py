2f8ef9eb104e5dd486494a731bce05f6cb40e702
anmello <95907305+anmello@users.noreply.github.com>
2023-06-26 11:08:16 +0200

    fixed magn print

--- main.py
+++ main.py
@@ -1,89 +1,104 @@
-# importing libraries 
+## here read the parameters and run the script. I will create a bash script to loop over different values
+# importing libraries
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
 
 # standard Qiskit libraries
 from qiskit import QuantumCircuit, transpile, Aer, IBMQ
-from qiskit.tools.jupyter import *
-from qiskit.visualization import *
-from ibm_quantum_widgets import *
 from qiskit.providers.aer import QasmSimulator
 from qiskit.opflow.primitive_ops import PauliSumOp
 from qiskit.providers.aer import AerSimulator
 from qiskit.opflow import Z,I,X
 from qiskit.circuit.library import EfficientSU2
 from qiskit.algorithms.optimizers import SPSA,  COBYLA
-from qiskit.providers.basicaer import QasmSimulatorPy  # local simulator 
+from qiskit.providers.basicaer import QasmSimulatorPy  # local simulator
 from qiskit.algorithms import NumPyMinimumEigensolver
 from qiskit.algorithms.minimum_eigensolvers import VQE
 import matplotlib.pyplot as plt
 from qiskit.providers.fake_provider import FakeGeneva, FakeVigo, FakeNairobi, FakeAuckland
 from qiskit_aer.noise import NoiseModel
 from qiskit_ibm_runtime import QiskitRuntimeService, Estimator, Session, Options
+import argparse
+import datetime
+
+'''
+   Read from config file.
+   USAGE example: python main.py -f 0 -J 1 -B 0.1 -n 5
+   f 0 noiseless simulation, 1 noisy simulation, 2 real backend), J (=1 antiferro, =-1 ferro), h (magnetic field) and nqubits (number of qubits in the system)
+ '''
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--flagnoise', type=int, required=True)
+parser.add_argument('-J', '--J', type=float, required=True)
+parser.add_argument('-B', '--magneticfield', type=float, required=True)
+parser.add_argument('-n', '--nqubits', type=int, required=True)
+
+args = parser.parse_args()
+
+print(args.flagnoise, args.J, args.magneticfield, args.nqubits)
+
+flag = int(args.flagnoise)
+
+J = float(args.J)
+
+h = float(args.magneticfield)
+
+nqubits = int(args.nqubits)
+
 
 # useful functions
-def ham_generator(N,h,J):
-    H=0
-    
+def ham_generator(N, h, J):
+    H = 0
+
     # field term
-    for i in range (N):
-        H+= h*((I^(i))^(Z)^(I^(N-i-1))) 
-    # interaction term
-    for k in range(N-1):
-        H+= J*((I^(k))^(X^X)^(I^(N-k-2)))
+    for i in range(N):
+        H += h * ((I ^ (i)) ^ (Z) ^ (I ^ (N - i - 1)))
+        # interaction term
+    for k in range(N - 1):
+        H += J * ((I ^ (k)) ^ (X ^ X) ^ (I ^ (N - k - 2)))
     # PBC
-    H+= J*((X)^(I^(N-2))^(X))           
-    return H    
+    H += J * ((X) ^ (I ^ (N - 2)) ^ (X))
+    return H
 
-#function that calculates the forward derivative
-def f_derivative(a,b,en,npoints):
 
-    step = (b-a)/(npoints-1)
+# function that calculates the forward derivative
+def f_derivative(a, b, en, npoints):
+    step = (b - a) / (npoints - 1)
     for i in range(npoints):
-        if a + i*step == 0:
+        if a + i * step == 0:
             print('zero found in position >>', i)
-            z=i
+            z = i
             break
-    fd = (en[z+1]-en[z])/(step)
-    
+    fd = (en[z + 1] - en[z]) / (step)
+
     print('spacing used to calculate the derivative >> ', step)
     print('numerical derivative of E(h), h->0+ >>', fd)
     return fd
 
 
-#function that calculates the backward derivative
-def b_derivative(a,b,en,npoints):
-
-    step = (b-a)/(npoints-1) 
+# function that calculates the backward derivative
+def b_derivative(a, b, en, npoints):
+    step = (b - a) / (npoints - 1)
     for i in range(npoints):
-        if a + i*step == 0:
+        if a + i * step == 0:
             print('zero found in position >>', i)
-            z=i
+            z = i
             break
-    bd = (en[z]-en[z-1])/(step)
-    
+    bd = (en[z] - en[z - 1]) / (step)
+
     print('spacing used to calculate the derivative >> ', step)
     print('numerical derivative of E(h), h->0- >>', bd)
     return bd
 
-#definition magnetization
+
+# definition magnetization
 def magnetization(N):
-    M=0
-    
+    M = 0
+
     # field term
     for i in range(N):
-        M+= ((I^(i))^(X)^(I^(N-i-1)))           
-    return M/N 
-
-
-flag = int(sys.argv[1])
-
-J = float(sys.argv[2])
-
-h = float(sys.argv[3])
-
-nqubits = int(sys.argv[4])
+        M += ((I ^ (i)) ^ (X) ^ (I ^ (N - i - 1)))
+    return M / N
 
 # Create an object to store intermediate results
 from dataclasses import dataclass
@@ -100,6 +115,8 @@ class VQELog:
 log = VQELog([],[])
 
 
+# Initial timestamp
+initial_timestamp = datetime.datetime.now()
 
 service = QiskitRuntimeService()
 backend = "ibmq_qasm_simulator"
@@ -110,32 +127,30 @@ optimizer = SPSA(maxiter=2000)
 
 initial_point = np.random.random(ansatz.num_parameters)
 
-
 if flag == 0:
-    
+    #NOISELESS
     with Session(service=service, backend=backend) as session:
-    
-        log = VQELog([],[])
+
+        log = VQELog([], [])
         vqe = VQE(Estimator(),
                   ansatz=ansatz, optimizer=optimizer, initial_point=initial_point, callback=log.update)
-        M=magnetization(nqubits)
-        H = ham_generator(nqubits,h,J)
-        result = vqe.compute_minimum_eigenvalue(H,  aux_operators=[M])
-   
-    #print("Experiment complete.".ljust(30))
-    #print(f"GS: {result.optimal_value}")
-    #print(f"Magn:{result.aux_operators_evaluated}")
-        
-        
-        tmp_title = str(flag)+"_"+str(J)+"_"+str(h)+"_"+"_"+str(nqubits)+".txt"
-        values = open(tmp_title,"w+")
-        print(result.optimal_value,file=values)
-        print(result.aux_operators_evaluated, file = values)
-        
+        M = magnetization(nqubits)
+        H = ham_generator(nqubits, h, J)
+        result = vqe.compute_minimum_eigenvalue(H, aux_operators=[M])
+
+        # print("Experiment complete.".ljust(30))
+        # print(f"GS: {result.optimal_value}")
+        # print(f"Magn:{result.aux_operators_evaluated}")
+
+        tmp_title = str(flag) + "_" + str(J) + "_" + str(h) + "_" + "_" + str(nqubits) + ".txt"
+        values = open(tmp_title, "w+")
+        print(result.optimal_value, file=values)
+        print((result.aux_operators_evaluated)[0][0], file=values)
+
         values.close()
 
 elif flag == 1:
-    
+    # NOISY
     with Session(service=service, backend=backend) as session:
         # Make a noise model
         fake_backend = FakeAuckland()
@@ -145,31 +160,67 @@ elif flag == 1:
         options = Options()
         options.simulator = {
             "noise_model": noise_model,
-            "basis_gates": fake_backend.configuration().basis_gates,
-            "coupling_map": fake_backend.configuration().coupling_map,
+            "coupling_map": fake_backend.coupling_map,
             "seed_simulator": 42
-            }
+        }
 
         # Set number of shots, optimization_level and resilience_level
-        options.execution.shots = 2000
+        options.execution.shots = 5000
         options.optimization_level = 3
         options.resilience_level = 2
-    
-        log = VQELog([],[])
-        vqe = VQE( Estimator(session=session,options=options),
+
+        log = VQELog([], [])
+        vqe = VQE(Estimator(session=session, options=options),
                   ansatz, optimizer, callback=log.update, initial_point=initial_point)
-        M=magnetization(nqubits)
-        H = ham_generator(nqubits,h,J)
-        result = vqe.compute_minimum_eigenvalue(H,  aux_operators=[M])
-        
-   
-    #print("Experiment complete.".ljust(30))
-    #print(f"GS: {result.optimal_value}")
-    #print(f"Magn:{result.aux_operators_evaluated}")
-    
-        tmp_title = str(flag)+"_"+str(J)+"_"+str(h)+"_"+"_"+str(nqubits)+".txt"
-        values = open(tmp_title,"w+")
-        print(result.optimal_value,file=values)
-        print(result.aux_operators_evaluated, file = values)
+        M = magnetization(nqubits)
+        H = ham_generator(nqubits, h, J)
+        result = vqe.compute_minimum_eigenvalue(H, aux_operators=[M])
+
+        # print("Experiment complete.".ljust(30))
+        # print(f"GS: {result.optimal_value}")
+        # print(f"Magn:{result.aux_operators_evaluated}")
+
+        tmp_title = str(flag) + "_" + str(J) + "_" + str(h) + "_" + "_" + str(nqubits) + ".txt"
+        values = open(tmp_title, "w+")
+        print(result.optimal_value, file=values)
+        print((result.aux_operators_evaluated)[0][0], file=values)
 
         values.close()
+
+elif flag == 2:
+    #TODO check here
+
+    # REAL HW
+    backend = service.backend("ibm_auckland")
+    with Session(service=service, backend=backend) as session:
+        # Make a noise model
+        #noise_model = NoiseModel.from_backend(backend)
+
+        # Set options to include the noise model
+        options = Options()
+
+        # Set number of shots, optimization_level and resilience_level
+        options.execution.shots = 8000
+        options.optimization_level = 3
+        options.resilience_level = 2
+
+        log = VQELog([], [])
+        vqe = VQE(Estimator(session=session, options=options),
+                  ansatz, optimizer, callback=log.update, initial_point=initial_point)
+        M = magnetization(nqubits)
+        H = ham_generator(nqubits, h, J)
+        result = vqe.compute_minimum_eigenvalue(H, aux_operators=[M])
+
+        # print("Experiment complete.".ljust(30))
+        # print(f"GS: {result.optimal_value}")
+        # print(f"Magn:{result.aux_operators_evaluated}")
+
+        tmp_title = str(flag) + "_" + str(J) + "_" + str(h) + "_" + "_" + str(nqubits) + ".txt"
+        values = open(tmp_title, "w+")
+        print(result.optimal_value, file=values)
+        print((result.aux_operators_evaluated)[0][0], file=values)
+
+        values.close()
+
+print(str(datetime.datetime.now() - initial_timestamp))
+
