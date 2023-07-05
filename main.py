d3755faa5cfdb1f5cb7f9cd9e2e0249830aece4b
grossim <michele.grossi89@gmail.com>
2023-07-05 14:50:12 +0200

    update run 4 less resources

--- main.py
+++ main.py
@@ -153,6 +153,7 @@ if flag == 0:
 
 elif flag == 1:
     print('NOISY')
+    #https://qiskit.org/ecosystem/ibm-runtime/how_to/noisy_simulators.html
     with Session(service=service, backend=backend) as session:
         # Make a noise model
         fake_backend = FakeAuckland()
@@ -162,14 +163,16 @@ elif flag == 1:
         options = Options()
         options.simulator = {
             "noise_model": noise_model,
-            #"coupling_map": fake_backend.coupling_map,
+            "basis_gates": fake_backend.configuration().basis_gates,
+            "coupling_map": fake_backend.configuration().coupling_map,
             "seed_simulator": 42
         }
 
         # Set number of shots, optimization_level and resilience_level
-        options.execution.shots = 3000
+
+        options.execution.shots = 2000
         options.optimization_level = 3
-        options.resilience_level = 2
+        options.resilience_level = 1 #1 T-Rex, ZNE 2
 
         log = VQELog([], [])
         vqe = VQE(Estimator(session=session, options=options),
