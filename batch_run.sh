27b36e143db9f1cd4966f9354781455fb46ac242
Michele Grossi <mgrossi@olqti-gpu-01.cern.ch>
2023-07-25 18:07:20 +0200

    update

--- batch_run.sh
+++ batch_run.sh
@@ -3,8 +3,10 @@
 f=1
 J=1
 n=6
+r=2
+i=500
 
 for B in -0.3 0 0.3
 do
-	python main.py -f $f -J $J -B $B -n $n  > output_log/output_${f}_${B}_${J}_${n}.txt &
+	python new_main.py -f $f -J $J -B $B -n $n -r $r -i $i > output_log/output_${f}_${B}_${J}_${n}_${r}_${i}.txt &
 done
